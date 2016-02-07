b68774ef27a5177a305735bfa31e9a74c92d800b
LiuYanshi <lantern.done@gmail.com>
2016-02-07 08:08:16 +0800

    add 404 page but not use, modify error page

--- routes/error.js
+++ routes/error.js
@@ -7,6 +7,11 @@ module.exports = function (app) {
         var err = new Error('Not Found');
         err.status = 404;
         next(err);
+
+        //res.render('404', {
+        //    message: err.message,
+        //    error: err
+        //});
     });
 
     // TODO 2016年02月03日21:00:16 在routes/article.js中出错了,怎么把错误传递到500的处理里面?
@@ -14,7 +19,8 @@ module.exports = function (app) {
     // no stacktraces leaked to user
     app.use(function (err, req, res, next) {
         console.log('500-errorHanlder');
-        res.status(err.status || 500);
+        //res.status(err.status || 500);
+        err.status= err.status || 500;
         res.render('error', {
             message: err.message,
             error: err
